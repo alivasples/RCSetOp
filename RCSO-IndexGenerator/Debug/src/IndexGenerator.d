2077a0726fdf726beaa63d2ef9b9099ac5459ec1
alivasples <aliv_asples1306@hotmail.com>
2021-01-07 04:15:06 -0500

    .

---
+++ RCSO-IndexGenerator/Debug/src/IndexGenerator.d
@@ -0,0 +1,124 @@
+src/IndexGenerator.o: ../src/IndexGenerator.cpp \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/RCSO-IndexGenerator/src/IndexGenerator.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/RCSO-IndexGenerator/src/API/ArboretumAPI.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stMetricTree.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stPageManager.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stPage.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stCommon.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stMetricAccessMethod.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stResult.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stTreeResult.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stTreeResult-inl.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stUtil.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stUtil-inl.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stQueryHint.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stTreeInformation.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stPlainDiskPageManager.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stCommonIO.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stDiskPageManager.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/CStorage.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/CNSCache.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stMemoryPageManager.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stSlimTree.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stSlimNode.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stSlimNode-inl.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stGenericPriorityQueue.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stGenericPriorityQueue-inl.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stMAMView.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stFastMapper.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stFastMapper-inl.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stSlimTree-inl.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/util/BasicArrayObject.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/hermes/EuclideanDistance.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/hermes/DistanceFunction.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/hermes/EuclideanDistance-inl.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/hermes/DTWDistance.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/hermes/DTWDistance-inl.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stBTree.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stBNode.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stBNode-inl.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stBTree-inl.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/RCSO-IndexGenerator/src/API/FixedString.h \
+ /home/alivasples/Documentos/PROJECTS/RCSetOp/RCSO-IndexGenerator/include/sfp/simple_file_parser.h
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/RCSO-IndexGenerator/src/IndexGenerator.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/RCSO-IndexGenerator/src/API/ArboretumAPI.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stMetricTree.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stPageManager.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stPage.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stCommon.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stMetricAccessMethod.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stResult.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stTreeResult.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stTreeResult-inl.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stUtil.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stUtil-inl.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stQueryHint.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stTreeInformation.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stPlainDiskPageManager.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stCommonIO.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stDiskPageManager.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/CStorage.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/CNSCache.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stMemoryPageManager.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stSlimTree.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stSlimNode.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stSlimNode-inl.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stGenericPriorityQueue.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stGenericPriorityQueue-inl.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stMAMView.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stFastMapper.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stFastMapper-inl.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stSlimTree-inl.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/util/BasicArrayObject.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/hermes/EuclideanDistance.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/hermes/DistanceFunction.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/hermes/EuclideanDistance-inl.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/hermes/DTWDistance.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/hermes/DTWDistance-inl.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stBTree.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stBNode.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stBNode-inl.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/Utils/arboretum/include/arboretum/stBTree-inl.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/RCSO-IndexGenerator/src/API/FixedString.h:
+
+/home/alivasples/Documentos/PROJECTS/RCSetOp/RCSO-IndexGenerator/include/sfp/simple_file_parser.h:
